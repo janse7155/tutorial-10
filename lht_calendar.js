e5392fef561e52334f2da720dfb1fbfb192141cb
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-02-07 10:35:54 -0600

    second commit

--- lht_calendar.js
+++ lht_calendar.js
@@ -40,6 +40,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
     var calendarHTML = "<table id='calendar_table'>";
     calendarHTML += calCaption(calDate);
+    calendarHTML += calWeekdayRow();
     calendarHTML += "</table>";
     return calendarHTML;
 }
@@ -48,5 +49,52 @@ function createCalendar(calDate) {
 function calCaption(calDate) {
     //monthName array contains the list of month names
     var monthName = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "Septemeber", "October", "November", "December"];
-    //
+    //Determines the current month
+    var thisMonth = calDate.getMonth();
+    //Determines the current year
+    var thisYear = calDate.getFullYear();
+    //Write the caption
+    return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+}
+
+/*Function to write a table row of weekday abreviations*/
+function calWeekdayRow() {
+   //Array of weekday abreviations
+   var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+   var rowHTML = "<tr>"
+   //Look through the dayName array
+   for (var i = 0; i < dayName.length; i++) {
+      rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+   }
+   rowHTML += "</tr>";
+   return rowHTML;
+}
+/*Function to calculate the number of days in the month*/
+function daysInMonth(calDate) {
+   //Array of days in each month
+   var dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
+   //Extract the four digit year and month value
+   var thisYear = calDate.getFullYear();
+   var thisMonth = calDate.getMonth();
+   //Revise the days in Febuary for leap years
+   if (thisYear % 4 === 0) {
+      if ((thisYear % 100 != 0) || (thisYear % 400 === 0)) {
+         dayCount[1] = 29;
+   }
+   }
+   //Return the number for days for the current month
+   return dayCount[thisMonth];
+}
+
+/*Function to write table rows for each day of the month */
+function calDays(calDate) {
+   //Determine the starting day of the month
+var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+var weekDay = day.getDay();
+   //Write blank cells preceeding the starting day
+var htmlCode ="<tr>";
+for (var i = 0; i < weekDay; i++) {
+   htmlCode += "<td></td>";
+}
+   //Write cells for each day of the month
 }
